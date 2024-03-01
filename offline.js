fbc10084f58c591807af25084b7408e136f7a41e
Alfzlfn <161667863+Alfzlfn@users.noreply.github.com>
2024-03-01 08:13:35 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1709168343,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/burung_3removebg-sheet0.png",
+		"images/burung_3removebg-sheet1.png",
+		"images/burung_3removebg-sheet2.png",
+		"images/tiangremovebg-sheet0.png",
+		"images/tiangremovebg2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
